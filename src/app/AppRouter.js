81289957da10ae9d822185edef8d26c0577af488
Imran Khan <imransilvake@gmail.com>
2019-01-06 10:23:06 +0100

    update

--- src/app/AppRouter.js
+++ src/app/AppRouter.js
@@ -4,15 +4,15 @@ import { Route, Switch, withRouter } from 'react-router-dom';
 
 // app
 import ENV from '../environment/index';
-import Home from "./system/frame/home/Home";
-import Component404 from "./system/frame/404/Component404";
+import Home from './system/frame/home/Home';
+import Component404 from './system/frame/404/Component404';
 
 class AppRouter extends Component {
 	render() {
 		return (
 			<Switch>
 				<Route exact path={ENV.ROUTING.HOME} component={Home}/>
-				<Route exact from='*' component={Component404}/>
+				<Route exact from="*" component={Component404}/>
 			</Switch>
 		);
 	}
